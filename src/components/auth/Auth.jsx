c8c4d6ba86c30da2f963656510cf0f0c1d16fa03
culi <culitif@tuta.io>
2021-08-25 21:25:01 -0700

    create log in/out functionality

--- src/components/auth/Auth.jsx
+++ src/components/auth/Auth.jsx
@@ -4,14 +4,20 @@ import useAuth from '../../state/useAuth.js';
 const Auth = () => {
 
   const { loading, currentUser, setUserLogin, setUserSignup, logOut } = useAuth();
+  const [accountExists, setAccountExists] = React.useState(true);
 
   const handleSubmit = e => {
     e.preventDefault();
-    setUserSignup({ 
-      email: e.target.email.value, 
-      name: e.target.name.value,
-      cohort: e.target.cohort.value,
-      password: e.target.password.value
+
+    const { email, password, name, cohort } = e.target;
+
+    // decide whether to sign up or log in
+    const action = accountExists ? setUserLogin : setUserSignup;
+    action({ 
+      email: email.value, 
+      name: name && name.value,
+      cohort: cohort && cohort.value,
+      password: password.value
     });
   };
 
@@ -20,23 +26,35 @@ const Auth = () => {
       className="Auth"
       onSubmit={handleSubmit}
     >
-      <input name="name" placeholder="name" type="text"/>
       <input name="email" placeholder="email" type="text"/>
+      {!accountExists && <>
+        <input name="name" placeholder="name" type="text"/>
+        <select defaultValue="21-03mar" name="cohort">
+          <option value="21-11nov">November 2021</option>
+          <option value="21-09sep">September 2021</option>
+          <option value="21-08aug">August 2021</option>
+          <option value="21-06jun">June 2021</option>
+          <option value="21-04apr">April 2021</option>
+          <option value="21-03mar">March 2021</option>
+          <option value="21-01jan">January 2021</option>
+        </select>
+      </>}
       <input name="password" placeholder="password" type="password"/>
-      <select name="cohort">
-        <option value="21-06jul">July 2021</option>
-        <option value="21-06jun">June 2021</option>
-        <option value="21-04apr">April 2021</option>
-        <option value="21-03mar">March 2021</option>
-      </select>
 
       {loading 
         ? <span>...</span>
-        : <button type="submit">do the thing</button>
+        : <>
+          <button type="submit">{accountExists ? 'Log In!' : 'Sign Up :)'}</button>
+          {Boolean(currentUser) && <button onClick={logOut}>Log Out</button>}
+        </>
       }
+      <span 
+        className="auth-toggle"
+        onClick={() => setAccountExists(!accountExists)}
+      >{accountExists ? 'Need' : 'Already have'} an account?</span>
     </form>
 
-    <span>{'' + currentUser}</span>
+    {currentUser && <span>Hello, {currentUser.name}</span>}
   </>;
 };
 
