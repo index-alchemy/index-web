73a4ad5d64e148b29b4a244d3b82eac96da095e1
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-08-27 15:04:10 -0400

    added route to home

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Landing from '../landing/Landing.jsx';
+import SprintList from '../home/SprintList';
 
 
 export default function App() {
@@ -10,9 +11,9 @@ export default function App() {
       {/* <Header /> */}
       <Switch>
         <Route exact path="/" component={Landing} />
-        {/* <Route exact path="/home" component={Home} />
-        <Route exact path="/sprints/:id" component={Sprint} />
-        <Route exact path="sprints/sprint_id/:id" component={Pitch} /> */}
+        <Route exact path="/home" component={SprintList} />
+        {/* <Route exact path="/sprints/:id" component={Sprint} /> */}
+        {/* <Route exact path="sprints/sprint_id/:id" component={Pitch} /> */}
         <Redirect to="/" />
       </Switch>
     </>
