d3ff36e43cc70608be29b57622fb40da7216c98b
Austin Summerlin <summerlin.austin@gmail.com>
2021-08-27 10:48:41 -0700

    add landing to router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import Auth from '../auth/Auth';
 import './App.css';
+import Landing from '../landing/Landing.jsx';
+
 
 export default function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/home" component={Home} />
+        {/* <Route exact path="/home" component={Home} />
         <Route exact path="/sprints/:id" component={Sprint} />
-        <Route exact path="sprints/sprint_id/:id" component={Pitch} />
+        <Route exact path="sprints/sprint_id/:id" component={Pitch} /> */}
         <Redirect to="/" />
       </Switch>
     </>
