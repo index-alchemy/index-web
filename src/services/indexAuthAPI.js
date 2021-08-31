df5e38a2a684a2ca6bb87d759b07b823ca21a0d3
culi <culitif@tuta.io>
2021-08-31 01:59:02 -0700

    logout and verify functionality

--- src/services/indexAuthAPI.js
+++ src/services/indexAuthAPI.js
@@ -1,33 +1,33 @@
 const API = 'http://localhost:7890/api/v1';
 
-const signUp = async credentials => {
-  return await fetch(`${API}/users/auth/signup`, {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+const request = async (method, url, body = null) => {
+  return await fetch(`${API}${url}`, {
+    method,
+    headers: body ? { 'Content-Type': 'application/json' } : {},
     credentials: 'include',
-    body: JSON.stringify(credentials)
+    body: body ? JSON.stringify(body) : null,
   })
-    .then(resp => resp.json())
-    .catch(err => console.log(err))
+    .then(res => res.json())
+    .catch(err => console.error(err))
   ;
 };
 
-const logIn = async credentials => {
-  return await fetch(`${API}/users/auth/login`, {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    credentials: 'include',
-    body: JSON.stringify(credentials)
-  })
-    .then(resp => resp.json())
-    .catch(err => console.log(err))
-  ;
-}
+const signUp = async credentials => 
+  await request('POST', '/users/signup', credentials)
+;
 
-export { signUp, logIn };
+const logIn = async credentials => 
+  await request('POST', '/users/auth/login', credentials)
+;
+
+const verify = async () => 
+  await fetch(`${API}/users/auth/verify`, { credentials: 'include' })
+    .then(res => res.ok)
+    .catch(err => console.error(err))
+;
+
+const logOut = async () => 
+  await request('GET', '/users/auth/logout')
+;
+
+export { signUp, logIn, verify, logOut };
