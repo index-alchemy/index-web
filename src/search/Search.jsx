82e3183c3926afee77c6f459e9884deaa26a0ae0
Austin Summerlin <summerlin.austin@gmail.com>
2021-09-01 15:19:14 -0700

    styling

--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -49,7 +49,7 @@ const Search = () => (
         searchAsYouType={true}
         className="searchBox"
         translations={{ placeholder: 'Search' }} />
-      <h1>💡</h1>
+      <h3>💡</h3>
       <Hits hitComponent={Hit} />
       <Pagination
         showFirst={true}
