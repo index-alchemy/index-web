d48357fb17c5b45339463adf0af8d7ad4fa43178
culi <culitif@tuta.io>
2021-09-03 02:03:16 -0700

    update styling

--- src/styles/useStyles.js
+++ src/styles/useStyles.js
@@ -1,15 +1,5 @@
 import { createUseStyles } from 'react-jss';
 
-const colors = {
-  snow: '#fffafa',          // background
-  quicksilver: '#a6a6a6',   // border, shadow
-  ivory: '#64645e',
-  charcoal: '#36454f',      // text-input
-  feldgrau: '#1e2421',      // text
-
-  bluetiful: '#00a1ff',     // links
-};
-
 const useCommonStyles = createUseStyles({
   page: {
     display: 'flex',
@@ -36,94 +26,49 @@ const useCommonStyles = createUseStyles({
       textAlign: 'center',
     }
   },
-  buttonDefault: {
-    background: 'transparent',
-    border: ['0.6px', 'solid', '#ABABAB'],
-    borderRadius: '0.25rem',
-    boxShadow: '0px 0px 2px 0.2px rgba(0, 0, 0, 0.31)',
-    color: '#696969',
-    fontSize: '1rem',
-    cursor: 'pointer',
-    padding: '0.4rem 0.8rem',
-    display: 'inline-block',
-    // see: https://tobiasahlin.com/blog/how-to-animate-box-shadow/
-    position: 'relative',
-    '&::after': {
-      display: 'inline-block',
-      top: '0',
-      left: '0',
-      content: '""',
-      width: '100%',
-      height: '100%',
-      position: 'absolute',
-      boxShadow: '0 1px 10px 0 #a6a6a6',
-      opacity: '0',
-      transition: 'opacity 0.2s',
-    },
-    '&:hover::after': {
-      opacity: '1'
-    }
-  },
-  buttonPrimary: {
-    background: 'transparent',
-    color: '#696969',
-    border: ['0.6px', 'solid', '#ABABAB'],
-    borderRadius: '0.25rem',
-    boxShadow: '0px 0px 2px 0.2px rgba(0, 0, 0, 0.31)',
-    fontSize: '1rem',
-    cursor: 'pointer',
-    padding: '0.4rem 0.8rem',
-    display: 'inline-block',
-    // see: https://tobiasahlin.com/blog/how-to-animate-box-shadow/
-    position: 'relative',
-    '&::after': {
-      display: 'inline-block',
-      top: '0',
-      left: '0',
-      content: '""',
-      width: '100%',
-      height: '100%',
-      position: 'absolute',
-      zIndex: '-1',
-      boxShadow: '0 1px 10px 0 #a6a6a6',
-      opacity: '0',
-      transition: 'opacity 0.2s',
-    },
-    '&:hover::after': {
-      opacity: '1'
-    }
-  },
-  textInputDefault: {
-    backgroundColor: '#fff',
-    color: '#36454f',
-    border: ['0.6px', 'solid', '#ABABAB'],
-    outline: 'none',
-    borderRadius: '0.25rem',
-    padding: '0.25rem 0.5rem',
-    fontSize: '1rem',
-  },
-  selectDefault: {
-    backgroundColor: '#0001',
-    color: '#64645e',
-    border: '1px solid #a6a6a6',
-    borderRadius: '0.25rem',
-    boxShadow: '0 1px 2px 0 #a6a6a6',
-    padding: '0.25rem 0.5rem',
-    fontSize: '1rem',
-  },
+  buttonPrimary: {},
   toggleText: {
     cursor: 'pointer',
     fontSize: '1rem',
-    color: '#3c69e7'
+    color: 'var(--color-text-secondary)'
+  }
+});
+
+const useHeaderStyles = createUseStyles({
+  header: {
+    background: 'var(--color-bg-header)',
+    borderBottom: '1px solid var(--color-border)',
+    color: 'var(--color-text)',
+    fontWeight: '500',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '6rem',
+    padding: '1rem 2rem',
+    '& h3 button': {
+      border: 'none',
+      boxShadow: 'none',
+      background: 'none',
+      '&:hover::after': { opacity: 0 }
+    },
+    '& a': {
+      color: 'inherit',
+      textDecoration: 'none'
+    },
+    '& nav': {
+      display: 'flex',
+      alignItems: 'center',
+      gap: '1rem',
+    }
   }
 });
 
 const useLandingPageStyles = createUseStyles({
   bulbBreak: {
+    borderTop: 'var(--break)',
     width: '100%',
     margin: '3rem 0',
     border: 'none',
-    borderTop: [1, 'solid', '#E5E5E5'],
     textAlign: 'center',
     overflow: 'visible',
     height: '0.5rem',
@@ -133,20 +78,19 @@ const useLandingPageStyles = createUseStyles({
       fontSize: '2rem',
       padding: '0 1rem',
       position: 'relative',
-      backgroundColor: '#FEFEFE',
+      backgroundColor: 'var(--color-bg)',
       top: '-1.5rem',
     }
   },
   heading: {
+    color: 'var(--color-text-title)',
     fontSize: '2.5rem',
     fontWeight: '1000',
     textAlign: 'center',
-    color: '#1f1f1fe4'
   },
   subheading: {
     fontSize: '1.5rem',
-    fontWeight: '400',
-    opacity: '0.6',
+    fontWeight: '250',
     textAlign: 'center'
   },
   recentPitches: {
@@ -154,25 +98,29 @@ const useLandingPageStyles = createUseStyles({
     flexDirection: 'column',
     alignItems: 'center',
     '& > h4': {
-      opacity: '0.8',
       fontSize: '1.15rem',
       fontWeight: '600',
       margin: 0
+    },
+    '& > ul': { 
+      gap: '1rem',
+      display: 'flex',
+      flexDirection: 'column'
     }
   }
 });
 
 const useAuthPageStyles = createUseStyles({
   authForm: {
+    border: 'var(--border)',
+    borderRadius: 'var(--radius-big)',
+    boxShadow: 'var(--box-shadow-big)',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     gap: '0.5rem',
     padding: '1rem 2rem',
     background: 'none',
-    border: [0.5, 'solid', '#a6a6a6'],
-    borderRadius: '0.5rem',
-    boxShadow: '0 0 0.25rem #0003',
     '& > *': {
       width: '100%',
     },
@@ -182,11 +130,11 @@ const useAuthPageStyles = createUseStyles({
     }
   },
   checkField: {
-    color: '#36454f',
+    color: 'var(--color-text-input)',
     padding: '0.2rem 0',
     fontSize: '1rem',
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'space-between'
   },
 });
 
@@ -200,6 +148,10 @@ const useHomePageStyles = createUseStyles({
     gap: '1rem'
   },
   cohortItem: {
+    background: 'var(--color-fg)',
+    border: 'var(--border)',
+    borderRadius: 'var(--radius-big)',
+    boxShadow: 'var(--box-shadow-big)',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -207,9 +159,6 @@ const useHomePageStyles = createUseStyles({
     textAlign: 'center',
     width: '100%',
     height: '100%',
-    border: [0.5, 'solid', '#a6a6a6'],
-    borderRadius: '0.5rem',
-    boxShadow: '0 0 0.25rem #0003',
     padding: '1rem'
   },
   cohortName: {
@@ -234,14 +183,15 @@ const useHomePageStyles = createUseStyles({
     height: '100%',
     gap: '0.5rem',
     '& > li': {
+      background: 'var(--color-bg-input)',
+      border: 'var(--border)',
+      borderRadius: 'var(--radius)',
       width: '90%',
       fontWeight: '500',
-      backgroundColor: '#fff',
-      border: [0.5, 'solid', '#a6a6a6'],
-      borderRadius: '0.25rem',
       cursor: 'pointer',
       position: 'relative',
       '&::after': {
+        boxShadow: 'var(--box-shadow-hover)',
         display: 'inline-block',
         top: '0',
         left: '0',
@@ -250,7 +200,6 @@ const useHomePageStyles = createUseStyles({
         width: '100%',
         height: '100%',
         position: 'absolute',
-        boxShadow: '0 1px 10px 0 #a6a6a6',
         opacity: '0',
         transition: 'opacity 0.2s',
       },
@@ -258,16 +207,15 @@ const useHomePageStyles = createUseStyles({
         opacity: '1'
       },
       '& > a': {
+        color: 'var(--color-text-input)',
         display: 'inline-block',
         width: '100%',
         height: '100%',
         padding: '0.5rem',
-        color: '#1e2421',
-        textDecoration: 'none',
+        textDecoration: 'none'
       },
       '& span:last-child': {
         float: 'right',
-        opacity: 0.75,
         fontWeight: '100'
       }
     }
@@ -276,25 +224,24 @@ const useHomePageStyles = createUseStyles({
 
 const useSprintPageStyles = createUseStyles({
   pitchList: {
-    listStyle: 'none',
-    padding: 0,
     width: '100%',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: '1rem'
   },
   addPitchForm: {
     '& > *': {
+      border: 'var(--border)',
+      borderRadius: 'var(--radius-big)',
+      boxShadow: 'var(--box-shadow-big)',
       width: '100%',
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
       justifyContent: 'center',
       gap: '0.5rem',
-      border: [0.5, 'solid', '#a6a6a6'],
-      borderRadius: '0.5rem',
-      boxShadow: '0 0 0.25rem #0003',
       padding: '1rem',
       '& > *': {
         width: '100%',
@@ -310,32 +257,29 @@ const useSprintPageStyles = createUseStyles({
     padding: 0,
     display: 'flex',
     flexWrap: 'wrap',
-    gap: '0.5rem 1rem',
+    gap: '1rem',
     justifyContent: 'left',
     '& > li': {
-      // fontSize: '1rem',
-      // border: '1px solid #a6a6a6',
-      // borderRadius: '0.25rem',
-      // boxShadow: '0 1px 2px 0 #a6a6a6',
-      padding: '0.5rem',
+      fontSize: '1rem',
       '& > div': {
         display: 'flex',
         flexWrap: 'wrap',
         alignItems: 'center',
         gap: '0.25rem',
         '& > label': {
+          color: 'var(--color-text-input)',
           fontWeight: '600',
           '&:after': { content: '":"' }
         },
         '& > span': {
+          background: 'var(--color-fg)',
+          border: 'var(--border)',
+          borderRadius: 'var(--radius)',
+          padding: '0.15rem 0.25rem',
           maxWidth: '12rem',
           textOverflow: 'ellipsis',
           whiteSpace: 'nowrap',
           overflow: 'hidden',
-          backgroundColor: '#fff',
-          border: '1px solid #a6a6a6',
-          borderRadius: '0.25rem',
-          padding: '0.15rem 0.25rem',
           textAlign: 'center',
         }
       }
@@ -343,10 +287,51 @@ const useSprintPageStyles = createUseStyles({
   }
 });
 
+const usePitchItemStyles = createUseStyles({
+  pitchItem: {
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: '0.5rem',
+    '& input': {
+      width: '3rem'
+    },
+    '& > div': {
+      background: 'var(--color-fg)',
+      border: 'var(--border)',
+      borderRadius: 'var(--radius-big)',
+      boxShadow: 'var(--box-shadow)',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: '1rem',
+      '& > a': {
+        color: 'var(--color-text-title)',
+        fontSize: '1.15rem',
+        textDecoration: 'none',
+        fontWeight: '600'
+      },
+      '& > *:last-child': {
+        color: 'var(--color-text-input)',
+        fontWeight: '420',
+        opacity: 0.9,
+        display: '-webkit-box',
+        '-webkit-line-clamp': 3,
+        '-webkit-box-orient': 'vertical',
+        overflow: 'hidden'
+      }
+    }
+  }
+});
+
 export default useCommonStyles;
 export {
+  useHeaderStyles,
   useLandingPageStyles,
   useAuthPageStyles,
   useHomePageStyles,
-  useSprintPageStyles
+  useSprintPageStyles,
+  usePitchItemStyles
 };
