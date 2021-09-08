c8412f12518224d7392517823889ab7b8e9c3009
culi <culitif@tuta.io>
2021-09-08 11:32:06 -0700

    use built-in styling for search component

--- src/components/landing/Search.jsx
+++ src/components/landing/Search.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import algoliasearch from 'algoliasearch';
+import PitchItem from '../sprint/PitchItem';
 
-const Search = () => {
+const searchClient = algoliasearch(
+  process.env.REACT_APP_ALGOLIA_APP_ID || 'N82H923VC5', 
+  process.env.REACT_APP_ALGOLIA_SEARCH_API_KEY || 'cfb98a5ccb9ee9a4c9ebe8f6892ae575'
+);
+const searchIndex = searchClient.initIndex('pitches');
+
+const Search = ({ query, setQuery }) => {
+
+  const [pitches, setPitches] = React.useState([]);
+
+  const handleChange = async e => {
+    e.preventDefault();
+    setQuery(e.target.value);
+
+    await searchIndex.search(e.target.value)
+      .then(res => res.hits)
+      .then(setPitches)
+    ;
+  }
+
   return <>
     <div className="Search">
+      <input 
+        type="search"
+        placeholder="Search"
+        onChange={handleChange}
+      />
+
+      {query && <>
+        <ul>
+          {pitches.map(pitch => 
+            <PitchItem key={pitch.objectID} pitch={pitch}/>
+          )}
+        </ul>
+      </>}
     </div>
   </>;
 };
